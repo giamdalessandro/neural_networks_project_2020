2baee256a7ac510f0d32edea399920c5d3714960
zascerta <giammarco.daless@gmail.com>
2020-07-26 17:14:30 +0200

    GPU check

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-keras.applications.VGG16(
+
+# GPU check
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+
+model = keras.applications.VGG16(
     include_top=True, weights='imagenet', input_tensor=None, input_shape=None,
     pooling=None, classes=1000, classifier_activation='softmax'
 )
 
-print("hello")
+print("Model loaded.")
