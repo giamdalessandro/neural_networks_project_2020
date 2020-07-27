89010c8f2baeff11d346fc35493f288cf29a95ec
balthier <luca799791@gmail.com>
2020-07-27 10:52:00 +0200

    f

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# TODO
+#   - Disentgled CNN
+#       - aggiungere loss filtri
+#   - Build decision trees
+
+
 keras.applications.VGG16(
     include_top=True, weights='imagenet', input_tensor=None, input_shape=None,
     pooling=None, classes=1000, classifier_activation='softmax'
