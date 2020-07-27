cd9008bdfcbd30905dce3b91350aa1db7cb833c3
zascerta <giammarco.daless@gmail.com>
2020-07-27 11:37:30 +0200

    pretrained model and layer test

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 # GPU check
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-
 model = keras.applications.VGG16(
     include_top=True, weights='imagenet', input_tensor=None, input_shape=None,
     pooling=None, classes=1000, classifier_activation='softmax'
 )
+print(model.summary())
 
-print("Model loaded.")
+layer = model.get_layer(name='block1_conv1')
+print(layer.weights)
