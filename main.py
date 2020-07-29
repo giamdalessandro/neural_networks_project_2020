724f17ee7535c5717371be8ddec2c6852be1b95c
balthier <luca799791@gmail.com>
2020-07-29 10:35:50 +0200

    added other papers

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ from tensorflow import keras
 # GPU check
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-# getting first 5 VGG16 pre-trained conv blocks without last max pool
+#   1. getting first 5 VGG16 pre-trained conv blocks without last max pool
 VGG16 = keras.applications.VGG16(
     include_top=False, weights='imagenet', input_tensor=None, input_shape=None,
     pooling=None, classes=1000, classifier_activation='softmax'
 )
 model = keras.Sequential(VGG16.layers[:-1])
 print(model.summary())
-
-
 print(">> Model loaded.")
+
+
+#   2. modificare filtri nel top conv-layer --> aggiungere maschere
+#   3. aggiungere un nuovo conv-layer con M=512 filtri --> ogni filtro è un tensore 3x3xM
+#   4. aggiungere maschere per i filtri del nuovo conv-layer
+#   5. usare gli stessi FC inizializzati random
