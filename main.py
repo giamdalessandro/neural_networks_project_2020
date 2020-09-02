cab63a89487363984cb37dc355667d1532a0283f
zascerta <giammarco.daless@gmail.com>
2020-09-02 11:24:24 +0200

    load data fixing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tensorflow.config import experimental
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D , Flatten, Dropout
-from load_cnn import load_keras
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import categorical_crossentropy
+from load_cnn import load_keras, load_dataeset
 
 # GPU check
 print("Num GPUs Available: ", len(experimental.list_physical_devices('GPU')))
@@ -30,9 +32,9 @@ for layer in net.layers[:-1]:  # just exclude last layer from copying
 block_mask_1 = Conv2D(filters=512, kernel_size=(3,3), strides=(1,1), padding="same", dilation_rate=(1,1),
                     activation="relu", name="block_mask1")  # da completare con magia nera ?!?!
 block_mask_1.add_weight(shape=(3,3,512,512), initializer="random_normal", trainable=True)
-#block_mask_1.add_loss()                                    # to build the interpretable tree?
+#block_mask_1.add_loss()        # to build the interpretable tree?
 
-model.add(block_mask_1)                                     # add block_mask to model
+model.add(block_mask_1)         # add block_mask to model
 model.add(MaxPool2D(name="block_pool", pool_size=(2,2),strides=(2,2)))         # add max pool layer
 
 
@@ -42,13 +44,33 @@ model.add(MaxPool2D(name="block_pool", pool_size=(2,2),strides=(2,2)))         #
 block_mask_2 = Conv2D(filters=4096, kernel_size=(3,3), strides=(1,1), padding="valid", dilation_rate=(1,1),
                     activation="relu", name="block_mask2")  # conv or fully-connected ?!?!
 block_mask_2.add_weight(shape=(7,7,512,4096), initializer="random_normal", trainable=True)
-#block_mask_2.add_loss()                                    # to build the interpretable tree?
+#block_mask_2.add_loss()        # to build the interpretable tree?
 
-model.add(block_mask_2)                                     # add block_mask_2 to model
-print(model.summary())
+model.add(block_mask_2)         # add block_mask_2 to model
 
 
 ''' 
     5. usare gli stessi FC inizializzati random 
 '''
-model.add(Dropout(rate=0.8))
+##model.add(Flatten())
+##model.add(Dropout(rate=0.8))
+#model.add(Dense(units=4096,activation="relu"))
+##model.add(Dropout(rate=0.8))
+#model.add(Dense(units=4096,activation="relu"))
+#model.add(Dense(units=2, activation="softmax"))
+#
+#print(model.summary())
+#
+#model.compile(
+#    optimizer= Adam(learning_rate=0.01),
+#    loss= categorical_crossentropy,
+#    metrics=["accuracy"]
+#)
+#train_generator, validation_generator = load_dataeset()
+#model.fit(
+#    train_generator,
+#    steps_per_epoch=50,
+#    epochs=10,
+#    validation_data=validation_generator,
+#    validation_steps=100
+#)
