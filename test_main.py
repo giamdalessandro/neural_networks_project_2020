8a332e4a0a4f60f07526339847d393570915baeb
zascerta <giammarco.daless@gmail.com>
2020-09-17 10:58:53 +0200

    style fixes

--- test_main.py
+++ test_main.py
@@ -15,10 +15,8 @@ MASK_LAYER  =    True
 '''
 1. loading pre-trained net from keras.Applications model, because VGG16_vd .mat file is not working...
 '''
-#model = load_keras()
-model = tf.keras.Sequential([
-    MaskLayer()
-])
+model = load_keras()
+
 
 '''
 2. TODO add loss for each of the 512 filters
@@ -35,19 +33,20 @@ if MASK_LAYER:
 '''
 4. add final pooling
 '''
-#model.add(MaxPool2D(name="max_pool", pool_size=(2,2), strides=(2,2), data_format="channels_last"))
-##print(model.summary())                         
-#model.trainable = False                         # we only train the top fully connected layers 
+model.add(MaxPool2D(name="max_pool", pool_size=(2,2), strides=(2,2), data_format="channels_last"))
+#print(model.summary())                         
+model.trainable = False                         # we only train the top fully connected layers 
+
 
 ''' 
 5. usare gli stessi FC di VGG16 inizializzati random 
 '''
-#model.add(Flatten())
-##model.add(Dropout(rate=0.8))
-#model.add(Dense(units=4096,activation="relu"))
-##model.add(Dropout(rate=0.8))
-#model.add(Dense(units=4096,activation="relu"))
-#model.add(Dense(units=31, activation="softmax"))
+model.add(Flatten())
+#model.add(Dropout(rate=0.8))
+model.add(Dense(units=4096,activation="relu"))
+#model.add(Dropout(rate=0.8))
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=31, activation="softmax"))
 
 print(model.summary())
 
@@ -55,11 +54,11 @@ print(model.summary())
 '''
 6. Compiling and training the model
 '''
-#model.compile(
-#    optimizer= Adam(learning_rate=0.001),
-#    loss= categorical_crossentropy,
-#    metrics=["accuracy"]
-#)
+model.compile(
+    optimizer= Adam(learning_rate=0.001),
+    loss= categorical_crossentropy,
+    metrics=["accuracy"]
+)
 
 if TRAIN:
     train_generator, validation_generator = load_dataeset(dataset='imagenet')
