445263fc696bc1bb1116a2b6587aa7ef4bb79a31
zascerta <giammarco.daless@gmail.com>
2020-09-23 13:34:09 +0200

    loading model in CPU mode

--- src/learn_tree.py
+++ src/learn_tree.py
@@ -2,7 +2,9 @@ import os
 import tensorflow as tf
 from maskLayer import *
 
-MASKED1 = "./models/masked1_binary_2_epochs_22_9_2020_17_19.h5"
+MASKED1 = "./models/masked1_binary_25_epochs_22_9_2020_20_6.h5"
 
-imported = tf.keras.models.load_model(MASKED1, custom_objects={'MaskLayer':MaskLayer()})
-print(imported.summary())
+
+with tf.device("/CPU:0"):
+    imported = tf.keras.models.load_model(MASKED1, custom_objects={"MaskLayer":MaskLayer()})
+    print(imported.summary())
