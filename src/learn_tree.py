cef0a419e7d313bd683565c9f4014dd45faf0743
zascerta <giammarco.daless@gmail.com>
2020-09-24 17:03:36 +0200

    g point found!!

--- src/learn_tree.py
+++ src/learn_tree.py
@@ -10,16 +10,13 @@ MASKED1 = "./models/masked1_no_dropout_binary_50_epochs_24_9_2020_14_7.h5"
 
 with tf.device("/CPU:0"):
     m_trained = tf.keras.models.load_model(MASKED1, custom_objects={"MaskLayer":MaskLayer()})
-    #print(m_trained.summary())
+    print(m_trained.summary())
 
     pred = m_trained.predict(load_def(fileid="n02355227_obj/img/img/00009.jpg"))
     print("Classification score: {}".format(pred))
 
 
-#intermediate_layer_model = Model(inputs=m_trained.input, outputs=m_trained.get_layer("max_pool").output)
-#mask_output = intermediate_layer_model.predict(load_def())
-
-temp_model = Model(inputs=m_trained.input, outputs=m_trained.get_layer("flatten_1").output)
+temp_model = Model(inputs=m_trained.input, outputs=m_trained.get_layer("flatten").output)
 flatten_output = temp_model.predict(load_def(fileid="n02355227_obj/img/img/00010.jpg"))
 
 def compute_g(model, inputs):
@@ -39,16 +36,7 @@ def compute_g(model, inputs):
         y = fc_3(fc_2(fc_1(inputs)))
         gradient = tape.gradient(y, fc_1.variables)
 
-    return tf.reduce_sum(gradient[0], axis=None)
+    return tf.reshape(tf.reduce_sum(gradient[0], axis=1), shape=(7,7,512))
 
 print(compute_g(m_trained, flatten_output))
-
-''' ...credits to Scardapane:
-with tf.GradientTape() as tape:
-   fc_output = network_up_to_fc(x)
-   y = fully_connected_layer(fc)
-   y_summed = tf.reduce_sum(y)
-   gradient = tape.gradient(y, fc_output)
-'''
-
 
