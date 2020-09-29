c53f3e823b885710c03a6f7d3d47654ae600d73c
rooosyf <mariarosaria-98@hotmail.it>
2020-09-29 18:58:46 +0200

    ancora prove

--- src/prove_g.py
+++ src/prove_g.py
@@ -12,14 +12,14 @@ g_sum = tf.math.add(g1, g2)
 #g = - op.linprog(-g_sum)
 
 print(op.linprog(-g_sum, bounds=(-1,1)))
-'''
+
 
 from pulp import *
 import tensorflow as tf
 import numpy
 
 class MultiDimensionalLpVariable:
-    def __init__(self, name, dimensions, low_bound, up_bound, cat):
+    def __init__(self, name, dimensions, low_bound, up_bound):
         self.name = name
         try:
             self.dimensions = (*dimensions,)
@@ -27,10 +27,6 @@ class MultiDimensionalLpVariable:
             self.dimensions = (dimensions,)
         self.low_bound = low_bound
         self.up_bound = up_bound
-        assert cat in pulp.LpCategories, 'cat must be one of ("{}").'.format(
-            '", "'.join(pulp.LpCategories)
-        )
-        self.cat = cat
         self.variables = self._build_variables_array()
         self.values = None
 
@@ -43,7 +39,7 @@ class MultiDimensionalLpVariable:
 
     def _define_variable(self, *index):
         name = "_".join(map(str, (self.name, *index)))
-        return pulp.LpVariable(name, self.low_bound, self.up_bound, self.cat)
+        return pulp.LpVariable(name, self.low_bound, self.up_bound)
 
     def evaluate(self):
         f = numpy.vectorize(lambda i: pulp.value(i))
@@ -55,15 +51,55 @@ g1 = [0.21, 0.17, -0.7, -0.2]
 g2 = [-0.21, 0.17, -0.7, -0.2]
 
 
-
-
 prob = LpProblem("test09", LpMaximize)
 
 g_sum = tf.math.add(g1, g2)
-g = MultiDimensionalLpVariable("g", 4, -1, 1, "Real")
+
+g = LpVariable.dicts("g", list(range(4)), -1, 1)
+for i in range(4):
+    g[i].lowBound = -1
+    g[i].upBound = 1
+
+prob += lpSum(g_sum[i]*g[i] for i in range(4)), "obj"
+prob += lpSum(g[i]*g[i] for i in range(4)) == 1, "c1"
+
+print(g)
+
+'''
+
+import tensorflow as tf
+import numpy as np
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
 
 
-prob += g_sum*g, "obj"
-prob += g*g == 1, "c1"
 
-print(value(g))
+g1 = [0.21, 0.17, -0.7, -0.2]
+g2 = [-0.21, 0.17, -0.7, -0.2]
+
+
+
+def objective(x):
+    g_sum = tf.math.add(g1, g2)
+    #for i in range(4):
+        #obj += -g_sum[i]*x[i]
+    return -g_sum[0]*x[0] - g_sum[1]*x[1]
+
+def constraint1(x):
+    sum_eq = 1.0
+    for i in range(4):
+        sum_eq = sum_eq - x[i]**2
+    return sum_eq
+
+n = 4
+x0 = np.zeros(n)
+
+b = (-1.0,1.0)
+bnds = ([b, b, b, b])
+#bounds = Bounds(-1, 1)
+con1 = {'type': 'eq', 'fun': constraint1}
+cons = ([con1])
+solution = minimize(objective, x0, bounds=bnds,constraints=cons)
+x = solution.x
+
+print(x)
