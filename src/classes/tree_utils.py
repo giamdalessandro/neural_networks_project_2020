2f0762dc1e76b54be8984fb050164ab141ea0b29
zascerta <giammarco.daless@gmail.com>
2020-10-03 20:10:09 +0200

    lasso giphs everywhere

--- src/classes/tree_utils.py
+++ src/classes/tree_utils.py
@@ -290,3 +290,27 @@ def txt_log(tree, start_time, path="./log.txt"):
         #f.write(tree.info())
     f.close()'''
     raise NotImplementedError
+
+
+def alpha_b(Xs, Y, lambda):
+    '''
+    Xs = #insieme di x
+    y = #y
+    '''
+    from sklearn.linear_model import Lasso, LogisticRegression
+    from sklearn.feature_selection import SelectFromModel
+
+    '''
+    lasso = Lasso()
+    parameters = {'alpha' : [0, 1]}
+    lasso_regressor = GridSearchCV(lasso, parameters, scoring= 'neg_mean_squared_error', cv =5)
+    lasso_regressor.fit(Xs, y)
+    print(lasso_regressor.best_params_)
+    '''
+
+    sel_ = SelectFromModel(LogisticRegression(C=1, penalty='l1'), threshold= lambda)
+    sel_.fit(Xs, np.ravel(Y,order='C'))
+    sel_.get_support()
+
+    removed_feats = X.columns[(sel_.estimator_.coef_ == 0).ravel().tolist()]
+    return removed_feats
